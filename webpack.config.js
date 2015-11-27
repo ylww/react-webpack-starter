2b631540fba77c285c96f36baf0156180cbb9088
songbaiyang <songbaiyang@gmail.com>
2015-11-27 10:03:35 +0800

    reformat

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,34 @@
-var webpack = require('webpack');
-var path = require('path');
+var webpack = require ('webpack');
+var path = require ('path');
 
 module.exports = {
-    entry: [
-        'webpack/hot/dev-server',
-        'webpack-dev-server/client?http://localhost:8080',
-        './index.js'
-    ],
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    './index.js'
+  ],
 
-    output: {
-        path: __dirname + '/dist',
-        filename: 'bundle.js',
-        publicPath: '/static/'  // Required for webpack-dev-server,对应的地址为/static/bundle.js
-    },
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js',
+    publicPath: '/static/'  // Required for webpack-dev-server,对应的地址为/static/bundle.js
+  },
 
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ],
+  plugins: [
+    new webpack.HotModuleReplacementPlugin ()
+  ],
 
-    module: {
-        preLoaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'standard'
-        }],
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /(node_modules|bower_components)/,
-            loaders: ['babel-loader?presets[]=es2015&presets[]=react'],
-            include: path.join(__dirname, '.')
-        }]
-    }
+  module: {
+    preLoaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'standard'
+    }],
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loaders: ['babel-loader?presets[]=es2015&presets[]=react'],
+      include: path.join(__dirname, '.')
+    }]
+  }
 };
